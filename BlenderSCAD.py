3887800dbe6df7771a85effe08830acf998d83d4
miguelitoelgrande <you@example.com>
2013-12-29 02:14:20 +0100

    some more library functions and demos

--- BlenderSCAD.py
+++ BlenderSCAD.py
@@ -32,6 +32,10 @@ import os
 import bpy
 import bpy_types
 
+#################################################################
+## BlenderSCAD core functionality
+#################################################################   
+
 mylayers = [False]*20
 mylayers[0] = True
 
@@ -274,13 +278,68 @@ def round_edges(width=0.1, segments=32, angle_limit=30, apply=False ,o=None):
 		bpy.ops.object.modifier_apply(apply_as='DATA', modifier='MyBevel')
 	o.name = 'rnd('+obj.name+')'   
 	return o
-	
+
 #################################################################
+## some additional library functions
+#################################################################   
+#TODO: translate more lib functions and eventually pack into separate files
+
+# translated from "shapes.scad"
+## size is the XY plane size, height in Z
+def hexagon(size, height):
+    boxWidth = size/1.75 
+    o = None   
+    for r in [-60, 0, 60]: 
+        tmp=rotate([0,0,r], cube([boxWidth, size, height], true))
+        o=union(tmp,o) # trick to overcome OpenSCADs implicit union :-)
+
+#hexagon(10,30)
+
+# size is the XY plane size, height in Z
+def octagon(size, height):
+  intersection(
+    cube([size, size, height], true)
+    , rotate([0,0,45], cube([size, size, height], true))
+  )
+
+#octagon(size=10,height=25)
+
+def ellipsoid(w, h, center = false):
+    scale([1, h/w, 1], sphere(r=w/2, center=center) )
+
+#ellipsoid(w=10,h=5, center=true)
+	
+# roundedBox -> only sides!!
+# size is a vector [w, h, d]
+def roundedBox(width, height, depth, radius):
+  o1= cube([width-2*radius,height,depth], true)
+  o2= cube([width,height-2*radius,depth], true)
+  o = union(o1,o2)
+  for x in [radius-width/2, -radius+width/2] :
+       for y in [radius-height/2, -radius+height/2] :
+            ot= translate([x,y,0] , cylinder(r=radius, h=depth, center=true))
+            o = union(o,ot)
+
+#roundedBox(width=10, height=5, depth=20, radius=2)
+
+
+#" official" rcube module from thingiverse rounded primitives
+def rcube(Size=[20,20,20],b=0.5):
+    tmp = None
+    for x in [-(Size[0]/2-b),(Size[0]/2-b)]:
+       for y in [-(Size[1]/2-b),(Size[1]/2-b)]:
+            for z in [-(Size[2]/2-b),(Size[2]/2-b)]:
+                tmp= union(translate([x,y,z], sphere(b) ), tmp)                
+    hull(tmp)
+
+
+#rcube([10,20,10],1)
+
+
+#################################################################
+## Tests
 #################################################################   
 	
-
-#rotate( (90*pi/180,0,90*pi/180), cylinder(h=10,r=3) )
-
 #cylinder(r=10,h=20)	
 #cylinder(r=10,h=20, center=true)
 
@@ -322,6 +381,16 @@ def OpenSCADtests():
 	#
 
 #OpenSCADtests()
+
+def HullDemo():
+    return hull (
+        translate([0,0,0], cylinder(r=4,h=4) )
+        ,translate([20,0,0], cylinder(r=4,h=4) )
+        ,translate([10,20,0], cylinder(r=4,h=4) )
+    )
+    
+#HullDemo()   
+
 
 def Demo1():
 	scale([3,3,3],
@@ -403,7 +472,17 @@ b=14 # holder height
 #FilamentHolderSimple(D,A,b)
 
 
+def pacman():
+    # strange: need to do it with two differences...
+    difference(
+    difference(
+        sphere(r=6)        
+    ,   translate([1,1,-6], cube([6,6,12])))
+    ,   translate([-1,4,+2], cylinder (r=1, h=3  ) )
+    ,   translate([-1,4,-5], cylinder (r=1, h=3  ) )
+    )
 
+#pacman()
 
 
 
